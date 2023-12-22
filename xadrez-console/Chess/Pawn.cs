b2c8b66818af2fced43dff931994a6bc7cadb7ba
Guilherme <schmitgui@gmail.com>
2023-12-22 18:20:52 -0300

    Implementacao da rainha, bispo e peao

---
+++ xadrez-console/Chess/Pawn.cs
@@ -0,0 +1,72 @@
+﻿using board;
+namespace chess
+{
+    public class Pawn : Piece
+    {
+        public Pawn(Board board, Color color) : base(board, color)
+        {
+        }
+        private bool canMove(Position pos)
+        {
+            Piece p = Board.piece(pos);
+
+            return p == null;
+        }
+
+        private bool canCapture(Position pos)
+        {
+            Piece p = Board.piece(pos);
+
+            return p != null && p.Color != this.Color;
+        }
+
+        public override bool[,] possibleMoviments()
+        {
+            bool[,] mat = new bool[Board.Line, Board.Column];
+
+            Position pos = new Position(0, 0);
+
+            if (Color == Color.White)
+            {
+                //N (norte)
+                pos.defineValues(this.Position.Line - 1, this.Position.Column);
+                if (Board.PositionValid(pos) && canMove(pos))
+                {
+                    mat[pos.Line, pos.Column] = true;
+                }
+            }
+            else
+            {
+                //S (sul)
+                pos.defineValues(this.Position.Line + 1, this.Position.Column);
+                if (Board.PositionValid(pos) && canMove(pos))
+                {
+                    mat[pos.Line, pos.Column] = true;
+                }
+            }
+            
+
+            //NO
+            pos.defineValues(this.Position.Line - 1, this.Position.Column - 1);
+            if (Board.PositionValid(pos) && canCapture(pos))
+            {
+                mat[pos.Line, pos.Column] = true;
+            }
+
+            //NE
+            pos.defineValues(this.Position.Line - 1, this.Position.Column + 1);
+            if (Board.PositionValid(pos) && canCapture(pos))
+            {
+                mat[pos.Line, pos.Column] = true;
+            }
+
+            return mat;
+        }
+
+        public override string ToString()
+        {
+            return "P";
+        }
+    }
+}
+
---
+++ xadrez-console/Chess/Queen.cs
@@ -0,0 +1,145 @@
+﻿using board;
+namespace chess
+{
+    public class Queen : Piece
+    {
+        public Queen(Board board, Color color) : base(board, color)
+        {
+        }
+
+        private bool canMove(Position pos)
+        {
+            Piece p = Board.piece(pos);
+
+            return p == null || p.Color != this.Color;
+        }
+
+        public override bool[,] possibleMoviments()
+        {
+            bool[,] mat = new bool[Board.Line, Board.Column];
+
+            Position pos = new Position(0, 0);
+
+            //N (norte)
+            pos.defineValues(this.Position.Line - 1, this.Position.Column);
+
+            while (Board.PositionValid(pos) && canMove(pos))
+            {
+                mat[pos.Line, pos.Column] = true;
+
+                if (Board.piece(pos) != null && Board.piece(pos).Color != this.Color)
+                {
+                    break;
+                }
+
+                pos.Line = pos.Line - 1;
+            }
+
+            //S
+            pos.defineValues(this.Position.Line + 1, this.Position.Column);
+            while (Board.PositionValid(pos) && canMove(pos))
+            {
+                mat[pos.Line, pos.Column] = true;
+
+                if (Board.piece(pos) != null && Board.piece(pos).Color != Color)
+                {
+                    break;
+                }
+
+                pos.Line = pos.Line + 1;
+            }
+
+            //L
+            pos.defineValues(this.Position.Line, this.Position.Column + 1);
+            while (Board.PositionValid(pos) && canMove(pos))
+            {
+                mat[pos.Line, pos.Column] = true;
+
+                if (Board.piece(pos) != null && Board.piece(pos).Color != Color)
+                {
+                    break;
+                }
+
+                pos.Column = pos.Column + 1;
+            }
+
+            //O
+            pos.defineValues(this.Position.Line, this.Position.Column - 1);
+            while (Board.PositionValid(pos) && canMove(pos))
+            {
+                mat[pos.Line, pos.Column] = true;
+
+                if (Board.piece(pos) != null && Board.piece(pos).Color != Color)
+                {
+                    break;
+                }
+
+                pos.Column = pos.Column - 1;
+            }
+
+            //Diagonal 1
+            pos.defineValues(this.Position.Line - 1, this.Position.Column - 1);
+            while (Board.PositionValid(pos) && canMove(pos))
+            {
+                mat[pos.Line, pos.Column] = true;
+
+                if (Board.piece(pos) != null && Board.piece(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.Line = pos.Line - 1;
+                pos.Column = pos.Column - 1;
+            }
+
+            //Diagonal 2
+            pos.defineValues(this.Position.Line - 1, this.Position.Column + 1);
+            while (Board.PositionValid(pos) && canMove(pos))
+            {
+                mat[pos.Line, pos.Column] = true;
+
+                if (Board.piece(pos) != null && Board.piece(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.Line = pos.Line - 1;
+                pos.Column = pos.Column + 1;
+            }
+
+            //Diagonal 3
+            pos.defineValues(this.Position.Line + 1, this.Position.Column + 1);
+            while (Board.PositionValid(pos) && canMove(pos))
+            {
+                mat[pos.Line, pos.Column] = true;
+
+                if (Board.piece(pos) != null && Board.piece(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.Line = pos.Line + 1;
+                pos.Column = pos.Column + 1;
+            }
+
+            //Diagonal 4
+            pos.defineValues(this.Position.Line + 1, this.Position.Column - 1);
+            while (Board.PositionValid(pos) && canMove(pos))
+            {
+                mat[pos.Line, pos.Column] = true;
+
+                if (Board.piece(pos) != null && Board.piece(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.Line = pos.Line + 1;
+                pos.Column = pos.Column - 1;
+            }
+
+            return mat;
+        }
+
+        public override string ToString()
+        {
+            return "Q";
+        }
+    }
+}
+
